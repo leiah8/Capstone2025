2546640b8e0a17edc17c84ac731a16cdc8fded05
Tony Lin <itsthetonylin@gmail.com>
2026-04-01 14:00:59 -0400

    removed old MyApp folder

--- MyApp/app/+not-found.tsx
+++
@@ -1,8 +0,0 @@
-import { View, Text } from 'react-native';
-export default function NotFound() {
-  return (
-    <View style={{ flex:1, alignItems:'center', justifyContent:'center' }}>
-      <Text>404 - Page not found</Text>
-    </View>
-  );
-}
--- MyApp/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  preset: 'jest-expo',
-  roots: ['<rootDir>/lib'],
-  testMatch: ['**/__tests__/**/*.test.ts'],
-  setupFiles: ['<rootDir>/lib/__tests__/setup.ts'],
-  clearMocks: true,
-};
